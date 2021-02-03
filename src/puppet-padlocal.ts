da54abe8996db5eea327b7970947eee42bf35041
haoda <oxddoxdd@gmail.com>
2021-02-03 16:37:10 +0800

    publis version 0.2.30

--- src/puppet-padlocal.ts
+++ src/puppet-padlocal.ts
@@ -1034,7 +1034,7 @@ class PuppetPadlocal extends Puppet {
 
   public async roomAdd(roomId: string, contactId: string): Promise<void> {
     const room = await this.roomPayload(roomId);
-    if (room.memberIdList.length > 50) {
+    if (room.memberIdList.length > 40) {
       await this._client!.api.inviteChatRoomMember(roomId, contactId);
     } else {
       await this._client!.api.addChatRoomMember(roomId, contactId);
