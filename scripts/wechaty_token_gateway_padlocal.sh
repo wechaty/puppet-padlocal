5a2c83ae5894acbdd333ce42e3043d195875c71c
haoda <oxddoxdd@gmail.com>
2021-03-15 14:58:22 +0800

    add wechaty_token_gateway_padlocal.sh

---
+++ scripts/wechaty_token_gateway_padlocal.sh
@@ -0,0 +1,23 @@
+if [ -z "$1" ]; then
+  >&2 echo -e "Missing argument.\nUsage: $0 <__padlocal_token__>"
+  exit 1
+fi
+
+WECHATY_PUPPET_PADLOCAL_TOKEN=$1
+WECHATY_PUPPET=wechaty-puppet-padlocal
+
+# Set port for your puppet service: must be published accessible on the internet
+WECHATY_PUPPET_SERVER_PORT=8788
+
+docker pull wechaty/wechaty
+
+docker run \
+--rm \
+-ti \
+-e WECHATY_LOG \
+-e WECHATY_PUPPET \
+-e WECHATY_PUPPET_PADLOCAL_TOKEN \
+-e WECHATY_PUPPET_SERVER_PORT \
+-e WECHATY_TOKEN="$WECHATY_PUPPET_PADLOCAL_TOKEN" \
+-p "$WECHATY_PUPPET_SERVER_PORT" \
+wechaty/wechaty
