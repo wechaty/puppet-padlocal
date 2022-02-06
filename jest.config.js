9e895496f4bdc07d440fb769e7c3e87b858ea503
Huan (李卓桓) <zixia@zixia.net>
2022-02-06 11:40:56 +0800

    add resolver and fix tsconfig

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   testRegex: "/tests/.*\\.(test|spec)?\\.(ts|tsx)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   setupFilesAfterEnv: ["./jest.setup.ts"],
+  resolver: "jest-ts-webcompat-resolver",
 };
