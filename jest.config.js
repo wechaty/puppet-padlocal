ea90997ecbbafa3b0b2f0e5f32f0d91a686ece51
haoda <oxddoxdd@gmail.com>
2022-04-28 23:59:09 +0800

    update jest version, to make test works

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  transform: { "^.+\\.ts?$": "ts-jest" },
+  preset: "ts-jest",
   testEnvironment: "node",
   testRegex: "/tests/.*\\.(test|spec)?\\.(ts|tsx)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
