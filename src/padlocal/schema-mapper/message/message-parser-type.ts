ad71287a52c3cad0b1f351c2bbd73b2694c81542
haoda <oxddoxdd@gmail.com>
2022-06-08 16:52:36 +0800

    refactor message-parser-type

--- src/padlocal/schema-mapper/message/message-parser-type.ts
+++ src/padlocal/schema-mapper/message/message-parser-type.ts
@@ -5,62 +5,31 @@ import { log } from "wechaty-puppet";
 import type { MessageParser, MessageParserContext } from "./message-parser.js";
 import { LOGPRE } from "./message-parser.js";
 
+const TypeMappings: { [key: number]: PUPPET.types.Message; } = {
+  [WechatMessageType.Text]: PUPPET.types.Message.Text,
+  [WechatMessageType.Image]: PUPPET.types.Message.Image,
+  [WechatMessageType.Voice]: PUPPET.types.Message.Audio,
+  [WechatMessageType.Emoticon]: PUPPET.types.Message.Emoticon,
+  [WechatMessageType.App]: PUPPET.types.Message.Attachment,
+  [WechatMessageType.File]: PUPPET.types.Message.Attachment,
+  [WechatMessageType.Location]: PUPPET.types.Message.Location,
+  [WechatMessageType.Video]: PUPPET.types.Message.Video,
+  [WechatMessageType.Sys]: PUPPET.types.Message.Unknown,
+  [WechatMessageType.ShareCard]: PUPPET.types.Message.Contact,
+  [WechatMessageType.VoipMsg]: PUPPET.types.Message.Recalled,
+  [WechatMessageType.SysTemplate]: PUPPET.types.Message.Recalled,
+  [WechatMessageType.StatusNotify]: PUPPET.types.Message.Unknown,
+  [WechatMessageType.SysNotice]: PUPPET.types.Message.Unknown,
+};
+
 export const typeParser: MessageParser = async(padLocalMessage: PadLocal.Message.AsObject, ret: PUPPET.payloads.Message, _context: MessageParserContext) => {
   const wechatMessageType = padLocalMessage.type as WechatMessageType;
-  let type: PUPPET.types.Message;
+  let type: PUPPET.types.Message | undefined = TypeMappings[wechatMessageType];
 
-  switch (wechatMessageType) {
-    case WechatMessageType.Text:
-      type = PUPPET.types.Message.Text;
-      break;
+  if (!type) {
+    log.verbose(LOGPRE, `unsupported type: ${JSON.stringify(padLocalMessage)}`);
 
-    case WechatMessageType.Image:
-      type = PUPPET.types.Message.Image;
-      break;
-
-    case WechatMessageType.Voice:
-      type = PUPPET.types.Message.Audio;
-      break;
-
-    case WechatMessageType.Emoticon:
-      type = PUPPET.types.Message.Emoticon;
-      break;
-
-    case WechatMessageType.App:
-    case WechatMessageType.File:
-      type = PUPPET.types.Message.Attachment;
-      break;
-
-    case WechatMessageType.Location:
-      type = PUPPET.types.Message.Location;
-      break;
-
-    case WechatMessageType.Video:
-      type = PUPPET.types.Message.Video;
-      break;
-
-    case WechatMessageType.Sys:
-      type = PUPPET.types.Message.Unknown;
-      break;
-
-    case WechatMessageType.ShareCard:
-      type = PUPPET.types.Message.Contact;
-      break;
-
-    case WechatMessageType.VoipMsg:
-    case WechatMessageType.SysTemplate:
-      type = PUPPET.types.Message.Recalled;
-      break;
-
-    case WechatMessageType.StatusNotify:
-    case WechatMessageType.SysNotice:
-      type = PUPPET.types.Message.Unknown;
-      break;
-
-    default:
-      log.verbose(LOGPRE, `unsupported type: ${JSON.stringify(padLocalMessage)}`);
-
-      type = PUPPET.types.Message.Unknown;
+    type = PUPPET.types.Message.Unknown;
   }
 
   ret.type = type;
